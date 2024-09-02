52abaa75c5973a63c7e371e06ed21404837ddc6e
Oleh Liubchenko <doroleijn@gmail.com>
2024-09-02 10:39:52 +0200

    images

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,40 +5,39 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Особливий геймплей',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    imgSrc: require('@site/static/img/winter.png').default,
     description: (
       <>
-        Кожного сезону на гравців очікують нові ігрові механіки.
+        Кожного сезону на сервер додаються ексклюзивні ігрові механіки, які доповнюють ванільний геймплей.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Кросплатформність',
+    imgSrc: require('@site/static/img/cat.png').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        На нашому сервері ви не обмежені у виборі пристрою для гри. Грайте хоч з холодильника чи годинника.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Історія сервера',
+    imgSrc: require('@site/static/img/library.png').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Всі масштабні ігрові події, які відбуваються на сервері, фіксуються на офіційний вікі. А також у вас є можливість створити сторінку про себе. 
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({imgSrc, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* Використовуємо тег img замість Svg */}
+        <img src={imgSrc} className={styles.featureImg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
